9a9a3246c3564aeaa47aa831ec8c22d9cf7e288a
Joseph Hooker <jcolehooker@gmail.com>
2021-09-12 15:21:24 -0500

    pre-Problem 2; copied the user input portion out of problem 1 to create inputArray() function for creating list with user input

--- for-loops-basic2.py
+++ for-loops-basic2.py
@@ -1,15 +1,31 @@
+#prologue - function for taking input from user
+def inputArray():
+    a = []
+    prompt_txt = "Please populate array with new number or provide invalid entry to quit: "
+    while True:
+        try:
+            user_input = int(input(prompt_txt))
+            a.append(user_input)
+        except:
+            break
+    return a
+
 #1-Biggie Size
 def biggieSize(arr):
     for i in range(len(arr)):
         if arr[i] > 0:
             arr[i] = "big"
     return arr
-a = []
-prompt_txt = "Please populate array with new number or provide invalid entry to quit: "
-while True:
-    try:
-        user_input = int(input(prompt_txt))
-        a.append(user_input)
-    except:
-        break
-print(biggieSize(a))
+print(biggieSize(inputArray()))
+
+# a = []
+# prompt_txt = "Please populate array with new number or provide invalid entry to quit: "
+# while True:
+#     try:
+#         user_input = int(input(prompt_txt))
+#         a.append(user_input)
+#     except:
+#         break
+# print(biggieSize(a))
+
+#2 - Sum Total
