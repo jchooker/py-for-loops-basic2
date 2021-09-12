3d323d24e9eda976b8a9356a5d5a821ceccbad55
Joseph Hooker <jcolehooker@gmail.com>
2021-09-12 15:50:53 -0500

    Finished problems 2 & 3

--- for-loops-basic2.py
+++ for-loops-basic2.py
@@ -1,4 +1,5 @@
 #prologue - function for taking input from user
+#Need version that will work after getting no input from user (i.e. length of 0)
 def inputArray():
     a = []
     prompt_txt = "Please populate array with new number or provide invalid entry to quit: "
@@ -11,12 +12,12 @@ def inputArray():
     return a
 
 #1-Biggie Size
-def biggieSize(arr):
-    for i in range(len(arr)):
-        if arr[i] > 0:
-            arr[i] = "big"
-    return arr
-print(biggieSize(inputArray()))
+# def biggieSize(arr):
+#     for i in range(len(arr)):
+#         if arr[i] > 0:
+#             arr[i] = "big"
+#     return arr
+# print(biggieSize(inputArray()))
 
 # a = []
 # prompt_txt = "Please populate array with new number or provide invalid entry to quit: "
@@ -28,4 +29,20 @@ print(biggieSize(inputArray()))
 #         break
 # print(biggieSize(a))
 
-#2 - Sum Total
+#2 - Count positives
+# def countPos(arr):
+#     posCt = 0
+#     for i in range(len(arr)):
+#         if arr[i] > 0:
+#             posCt+=1
+#     arr[len(arr)-1] = posCt
+#     return arr
+# print(countPos(inputArray()))
+
+#3 - Sum total
+def sumTotal(arr):
+    sum = 0
+    for i in arr:
+        sum+=i 
+    return sum 
+print(sumTotal(inputArray()))
